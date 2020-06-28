2ffbbf117dc95280da412260a8b61bb0780ae141
Kamal Pandey <kamalpandey484@gmail.com>
2020-06-28 20:18:33 +0530

    Added filter functionality

--- src/components/Cart/CartCourses.js
+++ src/components/Cart/CartCourses.js
@@ -2,19 +2,65 @@ import React, { Component } from "react"
 import Heading from "../Reusable/Heading"
 import Img from "gatsby-image"
 
+const getCaty = items => {
+  let holdItems = items.map(item => {
+    return item.node.category
+  })
+  let holdCategories = new Set(holdItems)
+  let categories = Array.from(holdCategories)
+  categories = ["All", ...categories]
+  return categories
+}
+
 export default class CartCourses extends Component {
   constructor(props) {
     super(props)
     this.state = {
       courses: props.courses.edges,
       filterredCourses: props.courses.edges,
+      myCategories: getCaty(props.courses.edges),
     }
   }
+
+  catyClicked = category => {
+    let courses = [...this.state.courses]
+    if (category === "All") {
+      this.setState(() => {
+        return { filterredCourses: courses }
+      })
+    } else {
+      let newFilterredCourses = courses.filter(
+        ({ node }) => node.category === category
+      )
+      this.setState(() => {
+        return { filterredCourses: newFilterredCourses }
+      })
+    }
+  }
+
   render() {
     return (
       <section className="py-5 ">
         <div className="container">
           <Heading title="Courses" />
+          <div className="row my-3">
+            <div className="col-10 mx-auto text-center">
+              {this.state.myCategories.map((category, index) => {
+                return (
+                  <button
+                    type="button"
+                    className="btn btn-info m-3 px-2"
+                    key={index}
+                    onClick={() => {
+                      this.catyClicked(category)
+                    }}
+                  >
+                    {category}
+                  </button>
+                )
+              })}
+            </div>
+          </div>
           <div className="row">
             {this.state.filterredCourses.map(({ node }) => {
               return (
